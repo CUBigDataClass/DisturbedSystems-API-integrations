28462b9cdb1d9866de36c0d56ed607a924fc27e0
Keval D Shah <keval.shah@colorado.edu>
2019-03-28 20:25:15 -0600

    Replaced with a Flask based server-app

--- dummyWebServer.py
+++ dummyWebServer.py
@@ -1,46 +1,92 @@
-#!/usr/bin/env python
-"""
-Very simple HTTP server in python.
-Usage::
-    ./dummy-web-server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-"""
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+from flask import Flask, render_template
+from flask_restful import Api, Resource, reqparse
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+app = Flask(__name__)
+api = Api(app)
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+users = [
+    {
+        "name": "Keval",
+        # "age": 42,
+        "location": "Brunswick, NJ"
+    },
+    {
+        "name": "Mrunal",
+        # "age": 32,
+        "location": "NY, NY"
+    },
+    {
+        "name": "Zoey",
+        # "age": 22,
+        "location": "Seattle, WA"
+    }
+]
 
-    def do_HEAD(self):
-        self._set_headers()
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
+
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        # parser.add_argument("age")
+        parser.add_argument("location")
+        args = parser.parse_args()
+
+        print (name)
+        print (args)
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            # "age": args["age"],
+            "location": args["location"]
+        }
+        users.append(user)
+        print (user)
+        return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        # parser.add_argument("age")
+        parser.add_argument("location")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                # user["age"] = args["age"]
+                user["location"] = args["location"]
+                return user, 200
         
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    httpd.serve_forever()
+        user = {
+            "name": name,
+            # "age": args["age"],
+            "location": args["location"]
+        }
+        users.append(user)
+        return user, 201
 
-if __name__ == "__main__":
-    from sys import argv
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
+
+api.add_resource(User, "/user/<string:name>")
+
+app.run(debug=True)
